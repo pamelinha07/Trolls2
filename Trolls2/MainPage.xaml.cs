5b74af050366eca0445bf439f192731dab6724bf
Pamela <pamela.isabeli167@gmail.com>
2024-07-30 09:00:20 -0300

    continua

--- Trolls2/MainPage.xaml.cs
+++ Trolls2/MainPage.xaml.cs
@@ -1,24 +1,15 @@
-﻿namespace Trolls2;
+﻿using System.ComponentModel;
+
+namespace Trolls2;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        // Example: Setting an image source
+        // Image image = new Image { Source = "scissors_and_thread.png" };
+        // Content.Children.Insert(1, image);
+    }
 }
-
